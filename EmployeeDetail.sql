cfcb46fd0671807200245dca5476a82467fad4c7
#Jakir0786 <125440621+jakir0786@users.noreply.github.com>
2023-04-11 04:37:23 -0700

    Update EmployeeDetail.sql

--- EmployeeDetail.sql
+++ EmployeeDetail.sql
@@ -141,11 +141,6 @@ where firstname not in ('vikas','ashish','nikhil');
 
 select convert(varchar(8), getdate(),108);
 
-select convert(varchar(8), month(),108);
-
-Select first name from "EmployeeDetail" table after removing 
-white spaces from right side
-
 select concat('','firstname')
 from employeedetail;
 
